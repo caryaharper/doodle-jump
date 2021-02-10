6c109da1f984bae49f5536d74632a15f24050a85
caryaharper <caryaharper@gmail.com>
2021-02-09 20:05:47 -0600

    Created function to create the plaforms of the game, and I positioned the doodler as well

--- app.js
+++ app.js
@@ -4,17 +4,43 @@ document.addEventListener('DOMContentLoaded', () => {
     let doodlerLeftSpace = 50;
     let doodlerBottomSpace = 150;
     let isGameOver = false;
+    let platformCount = 5;
+    const platforms = [];
 
     function createDoodler () {
         grid.appendChild(doodler);
         doodler.classList.add('doodler');
         doodler.style.left = doodlerLeftSpace + 'px';
-        doodler.style.bottom = doodlerBottomSpace + 'px' 
+        doodler.style.bottom = doodlerBottomSpace + 'px' ;
+    }
+
+    class Platform {
+        constructor(newPlatBottom) {
+            this.bottom = newPlatBottom;
+            this.left = Math.random() * 315;
+            this.visual = document.createElement('div');
+
+            const visual = this.visual;
+            visual.classList.add('platform');
+            visual.style.left = this.left + 'px';
+            visual.style.bottom = this.bottom + 'px';
+            grid.appendChild(visual);
+        }
+    }
+
+    function createPlatforms () {
+        for (let i = 0; i < platformCount; i++) {
+            let platGap = 600 / platformCount;
+            let newPlatBottom = 100 + i * platGap;
+            let newPlatform = new Platform(newPlatBottom);
+            platforms.push(newPlatform);
+        }
     }
 
     function start () {
         if (!isGameOver) {
             createDoodler();
+            createPlatforms();
         }
     }
     start();
